fb9405725f12975f29b212f17bb22f351664540f
Icelin <toimy4ka@gmail.com>
2023-12-20 20:38:27 +0300

    Update

---
+++ Ubuntu/configure.sh
@@ -0,0 +1,17 @@
+make install-git
+make configure-git
+make install-chrome
+make install-poetry
+make install-pyenv
+make install-python
+make install-telegram
+make install-discord
+make install-signal
+make install-zoom
+make install-obsidian
+make install-typora
+make install-gimp
+make install-obs
+make install-vscode
+make install-pycharm
+make install-docker
